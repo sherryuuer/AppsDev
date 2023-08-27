e43c7b1f50f631ec86d735b5a2a90d5d6953fed7
sherryuuer <41533280+sherryuuer@users.noreply.github.com>
2023-08-27 16:40:11 +0900

    Add files via upload

--- CoffeMachine/main.py
+++ CoffeMachine/main.py
@@ -7,9 +7,11 @@ DIME = 0.10
 NICKLE = 0.05
 PENNY = 0.01
 
+
 def user_order():
     """decide what to make next."""
     return input("What would you like? (espresso/latte/cappuccino/report): ")
+
 
 def check_resource(order):
     """check if there is enough resource for making a coffee."""
@@ -19,6 +21,7 @@ def check_resource(order):
             return False
     return True
 
+
 def check_transaction(order):
     """Check if the money insert is enough to pay the order."""
     print("Please insert coins.")
@@ -26,7 +29,10 @@ def check_transaction(order):
     dime_count = int(input("How many dimes? "))
     nickle_count = int(input("How many nickles? "))
     penny_count = int(input("How many pennies? "))
-    money_insert = QUARTER * quarter_count + DIME * dime_count + NICKLE * nickle_count + PENNY * penny_count
+    money_insert = QUARTER * quarter_count
+    money_insert += DIME * dime_count
+    money_insert += NICKLE * nickle_count
+    money_insert += PENNY * penny_count
     change = round((money_insert - MENU[order]["cost"]), 2)
     if change > 0:
         print(f"Here is ${change} in change.")
@@ -36,6 +42,7 @@ def check_transaction(order):
     else:
         print("There is not enough money.Money refund.")
         return False
+
 
 def make_coffee(order):
     """Make a coffee by deducte the ingredients of the order."""
@@ -58,7 +65,7 @@ while is_on:
             transaction_complate = check_transaction(order)
             if transaction_complate:
                 profit += MENU[order]["cost"]
-                make_coffee(order)            
+                make_coffee(order)
             else:
                 print("Please order again.")
         else:
