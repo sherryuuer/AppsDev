a0ec07c29c5afce48bd1b3b1cb81ed74c946892f
sherryuuer <41533280+sherryuuer@users.noreply.github.com>
2023-08-28 21:54:41 +0900

    Add files via upload

--- QuizProject/main.py
+++ QuizProject/main.py
@@ -1,9 +1,18 @@
 from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
 
 question_bank = []
 
 for q in question_data:
     question = Question(q["text"], q["answer"])
-    print(question.text, question.answer)
+    # print(question.text, question.answer)
     question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("You've complete the quiz.")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
